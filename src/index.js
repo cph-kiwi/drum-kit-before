004ff51e006a41d2f6875380022334112f2c10a8
Beth Jackson <beth.jack@gmail.com>
2020-09-01 10:19:27 +0200

    Adjusted set up folders Adjusted set up folders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import './style.css';
+import "./styles.css";
 
-document.getElementById('app').innerHTML = `
+document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
